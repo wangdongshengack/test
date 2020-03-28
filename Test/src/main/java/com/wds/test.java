04f10a0a572e84c68ed0f7b4cef84140e92ca72b
wangdongsheng <18236841678@163.com>
2020-03-28 18:39:39 +0800

    添加了一句话

--- Test/src/main/java/com/wds/test.java
+++ Test/src/main/java/com/wds/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hwllo word two");
+        System.out.println("hello word  three");
     }
 }
